ad4085240c0adc541fe114726aa871a57743451a
Mikhail-Kranyshev <91139884+Mikhail-Kranyshev@users.noreply.github.com>
2022-08-30 13:41:54 +0700

    Add files via upload

--- converter/Converter.java
+++ converter/Converter.java
@@ -1,55 +1,89 @@
 package converter;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Converter {
     private String number;
-    private int base;
+    private int sourceBase;
+    private int targetBase;
+
 
     private final Scanner scanner = new Scanner(System.in);
 
     public Converter() {
         number = "";
-        base = 10;
+        sourceBase = 10;
+        targetBase = 10;
     }
 
-    public void inputNumber(String msg) {
-        System.out.print(msg);
-        this.number = scanner.next();
+
+
+
+    private void convertingNumberFromBase10() {
+        BigInteger number = new BigInteger(this.number);
+        BigInteger targetBase = new BigInteger(String.valueOf(this.targetBase));
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!number.equals(BigInteger.ZERO)) {
+            int i = Integer.parseInt(String.valueOf(number.remainder(targetBase)));
+            if (i > 9) {
+                stringBuilder.append(Character.toChars(i + 55));
+            } else {
+                stringBuilder.append(number.remainder(targetBase));
+            }
+            number = number.divide(targetBase);
+        }
+        this.number = stringBuilder.reverse().toString();
     }
 
-    public void inputBase(String msg) {
-        System.out.print(msg);
-        base = scanner.nextInt();
+    private void convertingNumberToBase10() {
+        if (sourceBase != 10) {
+            char[] number = this.number.toUpperCase().toCharArray();
+            BigInteger bigInteger = BigInteger.ZERO;
+            BigInteger pow = BigInteger.ONE;
+            for (int i = number.length - 1; i >= 0; i--) {
+                BigInteger tmp = BigInteger.valueOf(number[i]);
+                if (number[i] > 64) {
+                    tmp = tmp.subtract(BigInteger.valueOf(55));
+                } else {
+                    tmp = tmp.subtract(BigInteger.valueOf(48));
+                }
+                bigInteger = bigInteger.add(tmp.multiply(pow));
+                pow = pow.multiply(BigInteger.valueOf(sourceBase));
+            }
+            this.number = bigInteger.toString();
+        }
     }
 
-    public void convertingNumberFromBase() {
-        inputNumber("Enter a number in decimal system: ");
-        inputBase("Enter the target base: ");
-        System.out.println("Conversion result: " +
-                switch (base) {
-                    case 2 -> Long.toBinaryString(Long.parseLong(number));
-                    case 8 -> Long.toOctalString(Long.parseLong(number));
-                    case 16 -> Long.toHexString(Long.parseLong(number)).toUpperCase();
-                    default -> number;
-                });
-    }
-
-    private void convertingNumberToBase() {
-        inputNumber("Enter source number: ");
-        inputBase("Enter source base: ");
-        System.out.println("Conversion to decimal result: " + Long.parseLong(number, base));
+    private void converting() {
+        while (true) {
+            System.out.printf("Enter number in base %d to convert to base %d (To go back type /back) ", sourceBase, targetBase);
+            number = scanner.nextLine();
+            if ("/back".equals(number)) {
+                return;
+            } else if (!"0".equals(number)) {
+                if (sourceBase != 10) {
+                    convertingNumberToBase10();
+                }
+                if (targetBase != 10) {
+                    convertingNumberFromBase10();
+                }
+            }
+            System.out.println("Conversion result: " + number + "\n");
+        }
     }
 
     public void action() {
         while (true) {
-            System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
-            switch (scanner.next()) {
-                case "/from" -> convertingNumberFromBase();
-                case "/to" -> convertingNumberToBase();
-                case "/exit" -> {
-                    return;
-                }
+            System.out.print("Enter two numbers in format: {source base} {target base} (To quit type /exit) ");
+            String string = scanner.nextLine();
+            if ("/exit".equals(string)) {
+                return;
+            } else {
+                String[] strings = string.split("\\s");
+                sourceBase = Integer.parseInt(strings[0]);
+                targetBase = Integer.parseInt(strings[1]);
+                converting();
             }
             System.out.println();
         }
--- converter/Main.java
+++ converter/Main.java
@@ -1,9 +1,11 @@
 package converter;
 
+
 public class Main {
 
     public static void main(String[] args) {
         Converter converter = new Converter();
         converter.action();
+
     }
 }
